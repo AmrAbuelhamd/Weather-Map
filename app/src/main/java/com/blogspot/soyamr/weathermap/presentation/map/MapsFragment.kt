c0cfff8648fb4c27f0f7c60db38aa6177be0b411
Amr Mohamed <37804517+AmrAbuelhamd@users.noreply.github.com>
2021-05-07 16:54:27 +0700

    Create map screen (#3)
    * .gitignore fix
    
    * Drop files from .gitignore
    
    * dropped weather-api file from gitignore
    
    * Drop files from .gitignore
    
    * dropped weather-api file from gitignore
    
    * Finished data.net package
    
    * changed single to factory in koin module
    
    * Fixed WeatherApiModule.kt.
    
    * Added: interceptors in data.interceptors; ConnectivityImpl.kt;
    
    * Removed empty lines.
    
    * Used nullable parameters for json response.
    
    * Added NoInternetException.kt
    
    * Updated evaluations
    
    * Added: permissions, location on map.
    
    * Improved code: added LocationManger.kt, DataBinding.
    
    * Removed title.
    
    * Added apk
    
    * Fixed wrong flag condition.
    
    * Added apk v2
    
    * code improvements.
    
    * Added IMapFragmentListener.kt; Refactoring LocationManger.kt;
    
    * reformatted code.
    
    * Code improvements.
    
    Co-authored-by: Amr Mokhamed <amr.abuelhamd@gmail.com>

--- app/src/main/java/com/blogspot/soyamr/weathermap/presentation/map/MapsFragment.kt
+++ app/src/main/java/com/blogspot/soyamr/weathermap/presentation/map/MapsFragment.kt
@@ -1,14 +1,27 @@
 package com.blogspot.soyamr.weathermap.presentation.map
 
-import androidx.fragment.app.Fragment
-
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import com.blogspot.soyamr.weathermap.R
-
+import com.blogspot.soyamr.weathermap.databinding.FragmentMapsBinding
+import com.blogspot.soyamr.weathermap.presentation.map.helpers.LocationListener
+import com.blogspot.soyamr.weathermap.presentation.map.helpers.LocationManger
+import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
@@ -16,32 +29,102 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private lateinit var binding: FragmentMapsBinding
+
+    private lateinit var googleMap: GoogleMap
+
+    private val locationManagerListener = object : LocationListener {
+        override fun onLocationUpdated(userLocation: Location) {
+            binding.progressCircular.isVisible = false
+            val userLocationLatLng = LatLng(userLocation.latitude, userLocation.longitude)
+            googleMap.addMarker(MarkerOptions().position(userLocationLatLng))
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(userLocationLatLng))
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(15F))
+        }
+
+        override fun hasLocationPermission() =
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+
+        override fun askUserToOpenGPS(exception: ResolvableApiException) {
+            val intentSenderRequest =
+                IntentSenderRequest.Builder(exception.resolution).build()
+            resolutionForResult.launch(intentSenderRequest)
+        }
+
+        override fun onSomethingWentWrong() {
+            showMessage(R.string.can_not_find_location, false)
+
+        }
+
+        override fun onGettingLocation() {
+            showMessage(R.string.getting_your_location, true)
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+    private val locationManger: LocationManger by lazy {
+        LocationManger(
+            requireContext(),
+            locationManagerListener
+        )
+    }
+
+    private val requestPermissionLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                locationManger.getUserLocation()
+            } else {
+                locationManagerListener.onSomethingWentWrong()
+            }
+        }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        this.googleMap = googleMap
+        if (locationManagerListener.hasLocationPermission()) {
+            locationManger.getUserLocation()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private val resolutionForResult: ActivityResultLauncher<IntentSenderRequest> =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { activityResult ->
+            if (activityResult.resultCode == RESULT_OK) {
+                locationManger.startLocationUpdates()
+            } else {
+                locationManagerListener.onSomethingWentWrong()
+            }
+        }
+
+
+    private fun showMessage(msgId: Int, showProgressBar: Boolean) {
+        binding.progressCircular.isVisible = showProgressBar
+        Toast.makeText(
+            requireContext(), getString(msgId), Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentMapsBinding.inflate(inflater).run {
+            binding = this
+            root
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        locationManger.stopLocationUpdates()
     }
 }
---
+++ app/src/main/java/com/blogspot/soyamr/weathermap/presentation/map/helpers/LocationManger.kt
@@ -0,0 +1,100 @@
+package com.blogspot.soyamr.weathermap.presentation.map.helpers
+
+import android.content.Context
+import android.content.IntentSender
+import android.os.Looper
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+
+class LocationManger(
+    private val context: Context,
+    private val locationListener: LocationListener
+) {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private val locationRequest: LocationRequest by lazy {
+        LocationRequest.create().apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    private val locationCallback: LocationCallback by lazy {
+        object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                var showErrorMessage = true
+                for (location in locationResult.locations) {
+                    if (location != null) {
+                        locationListener.onLocationUpdated(location)
+                        fusedLocationClient.removeLocationUpdates(this)
+                        showErrorMessage = false
+                        break
+                    }
+                }
+                if (showErrorMessage)
+                    locationListener.onSomethingWentWrong()
+            }
+        }
+    }
+
+    fun getUserLocation() {
+        try {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+            fusedLocationClient.lastLocation.addOnSuccessListener {
+                if (it != null) {
+                    locationListener.onLocationUpdated(it)
+                } else {
+                    requestLocationUpdates()
+                }
+            }
+        } catch (e: SecurityException) {
+            locationListener.onSomethingWentWrong()
+        }
+    }
+
+    private fun requestLocationUpdates() {
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(context)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { _ ->
+            startLocationUpdates()
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                try {
+                    locationListener.askUserToOpenGPS(exception)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    locationListener.onSomethingWentWrong()
+                }
+            }
+        }
+    }
+
+    fun startLocationUpdates() {
+        if (!locationListener.hasLocationPermission()) {
+            locationListener.onSomethingWentWrong()
+            return
+        }
+        locationListener.onGettingLocation()
+        try {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        } catch (e: SecurityException) {
+            locationListener.onSomethingWentWrong()
+        }
+    }
+
+    fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+}
