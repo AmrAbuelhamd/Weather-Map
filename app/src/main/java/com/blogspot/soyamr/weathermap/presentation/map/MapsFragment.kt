034d9ffa8b68faee08f19e1568cb09566b8f8209
Amr Mokhamed <amr.abuelhamd@gmail.com>
2021-05-11 00:39:02 +0700

    Got user click, showed city name.

--- app/src/main/java/com/blogspot/soyamr/weathermap/presentation/map/MapsFragment.kt
+++ app/src/main/java/com/blogspot/soyamr/weathermap/presentation/map/MapsFragment.kt
@@ -3,8 +3,13 @@ package com.blogspot.soyamr.weathermap.presentation.map
 import android.Manifest
 import android.app.Activity.RESULT_OK
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,6 +18,8 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.blogspot.soyamr.weathermap.R
@@ -24,8 +31,13 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import org.koin.android.ext.android.bind
+import java.util.*
+
 
 class MapsFragment : Fragment() {
 
@@ -87,6 +99,42 @@ class MapsFragment : Fragment() {
         } else {
             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
+        googleMap.setOnMapClickListener {
+            onUserClickOnMap(it)
+        }
+    }
+
+    private fun onUserClickOnMap(latLng: LatLng) {
+        googleMap.clear()
+        googleMap.addMarker(
+            MarkerOptions().position(latLng)
+                .icon(bitMapFromVector(R.drawable.ic_pin))
+        )
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15F))
+        showCityNameIfExists(latLng)
+    }
+
+    private fun showCityNameIfExists(latLng: LatLng) {
+
+        try {
+            val geocoder = Geocoder(requireContext())
+            val addresses: List<Address> =
+                geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+
+            if (addresses.isEmpty())
+                return//show nothing
+
+            val cityName: String = addresses[0].locality
+
+            binding.floatingCityNameContainer.isVisible = true
+            binding.cityNameTextView.text = cityName
+            binding.closeButton.setOnClickListener { binding.floatingCityNameContainer.isGone = true}
+            //button
+            Log.e("Hee","cityName:$cityName")
+
+        } catch (e: Exception) {
+            //show nothing
+        }
     }
 
     private val resolutionForResult: ActivityResultLauncher<IntentSenderRequest> =
@@ -98,13 +146,13 @@ class MapsFragment : Fragment() {
             }
         }
 
-
     private fun showMessage(msgId: Int, showProgressBar: Boolean) {
         binding.progressCircular.isVisible = showProgressBar
         Toast.makeText(
             requireContext(), getString(msgId), Toast.LENGTH_SHORT
         ).show()
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -126,5 +174,23 @@ class MapsFragment : Fragment() {
     override fun onPause() {
         super.onPause()
         locationManger.stopLocationUpdates()
+    }
+
+    private fun bitMapFromVector(vectorResID: Int): BitmapDescriptor {
+        val vectorDrawable = ContextCompat.getDrawable(requireContext(), vectorResID)
+        vectorDrawable!!.setBounds(
+            0,
+            0,
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight
+        )
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 }
