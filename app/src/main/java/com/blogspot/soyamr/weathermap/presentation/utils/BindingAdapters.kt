caee178ec8270fab06e57c5292100dcef6aa14aa
Amr <Amr>
2021-05-26 15:59:25 +0700

    Code improvements.

--- app/src/main/java/com/blogspot/soyamr/weathermap/presentation/utils/BindingAdapters.kt
+++ app/src/main/java/com/blogspot/soyamr/weathermap/presentation/utils/BindingAdapters.kt
@@ -2,7 +2,6 @@ package com.blogspot.soyamr.weathermap.presentation.utils
 
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import com.blogspot.soyamr.weathermap.R
 import com.bumptech.glide.Glide
@@ -21,12 +20,10 @@ fun setImage(image: ImageView, url: String?) {
 @BindingAdapter("app:windSpeed", "app:windDirection")
 fun setWindSpeed(view: TextView, windSpeed: String?, windDirectionResourceId: Int?) {
     if (windDirectionResourceId != null && windDirectionResourceId != 0 && !windSpeed.isNullOrBlank()) {
-        view.text = HtmlCompat.fromHtml(
-            view.context.getString(
-                R.string.wind_speed,
-                view.context.getString(windDirectionResourceId),
-                windSpeed
-            ), HtmlCompat.FROM_HTML_MODE_COMPACT
+        view.text = view.context.getString(
+            R.string.wind_speed,
+            view.context.getString(windDirectionResourceId),
+            windSpeed
         )
     }
 }
@@ -43,11 +40,9 @@ fun setHumidity(view: TextView, humidity: String?) {
 @BindingAdapter("app:pressure")
 fun setPressure(view: TextView, pressure: String?) {
     if (pressure != null) {
-        view.text = HtmlCompat.fromHtml(
-            view.context.getString(
-                R.string.pressure_unit,
-                pressure,
-            ), HtmlCompat.FROM_HTML_MODE_COMPACT
+        view.text = view.context.getString(
+            R.string.pressure_unit,
+            pressure,
         )
     }
 }
